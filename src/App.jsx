92158bcddce8be540e4e736374c343320db1a9ab
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-08 17:39:24 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing"
 export default function App() {
-    const x = 23;
-    return <div>WorldWise {x}</div>;
+    return <BrowserRouter>
+    <Routes>
+      <Route path="product" element={<Product/>}/>
+      <Route path="pricing" element={<Pricing/>}/>
+    </Routes>
+    </BrowserRouter>
 }
