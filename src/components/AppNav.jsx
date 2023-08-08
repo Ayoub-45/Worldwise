0d16e0d1383aaee76a3913c3b812919bfd7e34e3
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-08 21:56:13 +0200

    Create nested routes in the AppLayout

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router-dom"
+import styles from "./AppNav.module.css"
 export default function AppNav(){
     return(
-       <nav>
-       <h1>Hello</h1>
+       <nav className={styles.nav}>
+       <ul>
+        <li>
+            <NavLink to="cities"> Cities</NavLink>
+
+        </li>
+        <li>
+            <NavLink to="countries">Countries</NavLink>
+        </li>
+       </ul>
        </nav> 
     )
 }
