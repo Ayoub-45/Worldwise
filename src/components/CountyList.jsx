311c842aa97be23d6f2c1316dfd9df2007915872
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-09 13:32:08 +0200

    Implement countryList

--- src/components/CountyList.jsx
+++ src/components/CountyList.jsx
@@ -1,1 +1,21 @@
 import styles from "./CountryList.module.css"
+import Message from "./Message"
+import Spinner from "./Spinner"
+import CountryItem from "./CountryItem"
+export default function CityList({cities,isLoading}){
+    if(isLoading) return <Spinner/>
+    const countries=cities.reduce((arr,city)=> 
+        {
+            if(!arr.map(el=>el.country).includes(city.country))
+            return [...arr,{country:city.country,emoji:city.emoji}]
+            else return arr
+        },[])
+    if(!countries.length) return <Message message="Add your first city by clicking on a city on the map"/>
+    return(
+        <ul className={styles.countryList}>
+            {countries.map(country=>{
+                return <CountryItem  country={country}/>
+            })}
+        </ul>
+    )
+}
